380cbc012f96e756b75f28d3c69f857b4a20f034
Dario Russo <dario@dariorusso.ca>
2018-08-26 03:39:13 -0400

    fill out word service functionality

--- word_service.go
+++ word_service.go
@@ -2,6 +2,7 @@ package langmap
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,26 +12,71 @@ type WordService struct {
 }
 
 func (s *WordService) Create(c *gin.Context) {
-	f := Word{}
+	w := Word{}
 
-	if err := c.ShouldBindJSON(&f); err != nil {
+	if err := c.ShouldBindJSON(&w); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	s.Engine.DB.Create(&f)
+	// TODO(dario) check for errors
+	s.Engine.DB.Create(&w)
 
 	c.JSON(http.StatusCreated, nil)
-	return
 }
 
 func (s *WordService) Delete(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 0)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	w := Word{}
+	w.ID = uint(id)
+
+	// TODO(dario) check for errors
+	// this won't delete definitions. should it?
+	// it probably should.
+	s.Engine.DB.Delete(&w)
+
+	c.JSON(http.StatusNoContent, nil)
 }
 
 func (s *WordService) Get(c *gin.Context) {
+	w := make([]Word, 0)
+	db := s.Engine.DB
+
+	if _, ok := c.GetQuery("preload"); ok {
+		db = db.Preload("Definitions")
+	}
+
+	// TODO(dario) check for errors
+	db.Find(&w)
+
+	c.JSON(http.StatusOK, w)
+	return
 }
 
 func (s *WordService) GetOne(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 0)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	w := Word{}
+	db := s.Engine.DB
+
+	if _, ok := c.GetQuery("preload"); ok {
+		db = db.Preload("Definitions")
+	}
+
+	// TODO(dario) check for errors
+	db.Find(&w, id)
+
+	c.JSON(http.StatusOK, w)
+	return
 }
 
 func (s *WordService) Update(c *gin.Context) {

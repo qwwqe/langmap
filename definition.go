a6b980df9aa122548f994baf221fb8b830aed40a
felix <f.werth@gmail.com>
2018-09-15 10:54:46 +0800

    Retrieve or create Word before creation of Definition

--- definition.go
+++ definition.go
@@ -9,3 +9,27 @@ type Definition struct {
 
 	Word string `json:"word" db:"-"`
 }
+
+// (d *Definition) PreInsert retrieves or creates a Word before the
+// creation of a Definition and fills in that Definition's corresponding
+// WordId field
+func (d *Definition) PreInsert(s gorp.SqlExecutor) error {
+	var word Word
+	err := s.SelectOne(&word, "select * from words where word=?", d.Word)
+
+	switch err {
+	case nil: // Word exists, simply update WordId
+		d.WordId = word.Id
+	case sql.ErrNoRows: // Word does not exist, create it first
+		word = Word{Word: d.Word}
+		err := s.Insert(&word)
+		if err != nil {
+			return err
+		}
+		d.WordId = word.Id
+	default:
+		return err
+	}
+
+	return nil
+}

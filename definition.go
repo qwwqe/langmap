0b1346baf8fe3eacb24d2e2c96f2a7f1620b1e6f
Dario Russo <dario@dariorusso.ca>
2018-09-15 02:18:27 +0000

    Add JSON and DB tags

--- definition.go
+++ definition.go
@@ -1,9 +1,11 @@
 package langmap
 
 type Definition struct {
-	Id            uint
-	Pronunciation string
-	Meaning       string
-	WordId        uint
-	InstanceId    uint
+	Id            uint   `json:"id" db:"id"`
+	Pronunciation string `json:"pronunciation" db:"pronunciation"`
+	Meaning       string `json:"meaning" db:"meaning"`
+	WordId        uint   `json:"word_id" db:"word_id"`
+	InstanceId    uint   `json:"instance_id" db:"instance_id"`
+
+	Word string `json:"word" db:"-"`
 }

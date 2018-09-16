20e1a4138118f8f45f2641f8ea660aeca6a4be8b
Dario Russo <dario@dariorusso.ca>
2018-09-16 23:21:54 +0000

    refactor for ease of legibility

--- definition.go
+++ definition.go
@@ -16,26 +16,24 @@ type Definition struct {
 	Word string `json:"word" db:"-"`
 }
 
-// (d *Definition) PreInsert retrieves or creates a Word before the
-// creation of a Definition and fills in that Definition's corresponding
-// WordId field
 func (d *Definition) PreInsert(s gorp.SqlExecutor) error {
-	var word Word
-	err := s.SelectOne(&word, "select * from words where word=?", d.Word)
+	w := Word{}
 
-	switch err {
-	case nil: // Word exists, simply update WordId
-		d.WordId = word.Id
-	case sql.ErrNoRows: // Word does not exist, create it first
-		word = Word{Word: d.Word}
-		err := s.Insert(&word)
-		if err != nil {
+	if err := s.SelectOne(&w, "select * from words where word = $1", d.Word); err != nil {
+		if err == sql.ErrNoRows {
+			w.Word = d.Word
+
+			if err := s.Insert(&w); err != nil {
+				return err
+			}
+
+		} else {
 			return err
+
 		}
-		d.WordId = word.Id
-	default:
-		return err
 	}
+
+	d.WordId = w.Id
 
 	return nil
 }
